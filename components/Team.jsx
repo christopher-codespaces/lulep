50d2e488ea104023809323c8c16a0b1b1f436700
Christopher Makombe <125880140+christopher-codespaces@users.noreply.github.com>
2023-10-07 11:33:49 +0200

    Update Team.jsx

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,74 +1,92 @@
 import React from "react";
+import Image from "next/image";
+import christopher from "../assets/christopher.jpg";
+import nono from "../assets/nono.jpg";
+import mom from "../assets/mom.jpg";
 
+const Team = () => {
+  return (
+    <>
+      <section>
+        <h1
+          style={{
+            fontSize: 34,
+            textAlign: "center",
+            fontWeight: "900",
+            marginTop: 150,
+          }}
+        >
+          Team
+        </h1>
+        <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-7xl">
+          <div className="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-3">
+            <div className="p-6">
+              <Image
+                className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl"
+                src={christopher}
+                alt="Founder"
+                loading="lazy"
+                layout="responsive"
+              />
 
-const Team =()=>{
-    const team = [
-        {
-          avatar: "https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
-          name: "Martiana Dialan",
-          title: "Program Director",
-        },
-        {
-          avatar: "https://images.unsplash.com/photo-1623605931891-d5b95ee98459?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=640&q=80",
-          name: "Michael Colorand",
-          title: "Community Outreach Coordinator",
-        },
-        {
-          avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-          name: "Brown Luis",
-          title: "Fundraising Manager",
-        },
-        {
-          avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-          name: "Lysa Sandiago",
-          title: "Education Coordinator",
-        },
-        {
-          avatar: "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-          name: "Daniel Martin",
-          title: "Volunteer Coordinator",
-        },
-        {
-          avatar: "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-          name: "Vicky Tanson",
-          title: "Communications Manager",
-        },
-      ];
-    return (
-        <section className="py-14">
-            <div className="max-w-screen-xl mx-auto px-4 md:px-8">
-                <div className="max-w-xl mx-auto sm:text-center">
-                    <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
-                        Our team
-                    </h3>
-                    <p className="text-gray-600 mt-3">
-                        Meet the team behind the cause.
-                    </p>
-                </div>
-                <div className="mt-12">
-                    <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
-                        {
-                            team.map((item, idx) => (
-                                <li key={idx}>
-                                    <div className="w-full h-60 sm:h-52 md:h-56">
-                                        <img
-                                            src={item.avatar}
-                                            className="w-full h-full object-cover object-center shadow-md rounded-xl"
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div className="mt-4">
-                                        <h4 className="text-lg text-gray-700 font-semibold">{item.name}</h4>
-                                        <p className="text-indigo-600">{item.title}</p>
-                                    </div>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
+              <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">
+                Christopher Makombe
+              </h1>
+              <h7> Founder</h7>
+              <p className="mx-auto text-base leading-relaxed text-gray-500">
+                Hey ,My name is Christopher and I am the founder of SwiftlyBox.
+                I started SwiftlyBox to help my favorite restaurant and me doing
+                that everyday brings joy to my eyes.
+              </p>
+
+              <div className="mt-4"></div>
             </div>
-        </section>
-    )
-}
+            <div className="p-6">
+              <Image
+                className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl"
+                src={nono}
+                alt="Nothando"
+                loading="lazy"
+                layout="responsive"
+              />
+
+              <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">
+                Nothando Mapisa
+              </h1>
+              <h7> Accountant</h7>
+              <p className="mx-auto text-base leading-relaxed text-gray-500">
+                I work for my brothers tech startup because I see the passion in
+                his eyes when trying to solve his problem his solving .
+              </p>
+
+              <div className="mt-4"></div>
+            </div>
+            <div className="p-6">
+              <Image
+                className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl"
+                src={mom}
+                alt="Loveness Mahwire"
+                loading="lazy"
+                layout="responsive"
+              />
+
+              <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">
+                Loveness Mahwire
+              </h1>
+              <h7>Consultant</h7>
+              <p className="mx-auto text-base leading-relaxed text-gray-500">
+                I am proud of my son for reaching his goal ,he wanted to reach
+                .He will always be a inspiration to me .I always say that he
+                will kill himself until he helps a lot of people succeed.
+              </p>
+
+              <div className="mt-4"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Team;
